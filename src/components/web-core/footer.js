aa0d6fa7e19630534cad0b8673dc30d70c3c6aff
Zabdiel Maestre <zabdielmaestre@gmail.com>
2021-08-24 23:10:24 -0500

    first commit

---
+++ src/components/web-core/footer.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import styled from "styled-components"
+
+const FooterSite = styled.header`
+  color: green;
+`
+
+const Footer = () => (
+  <FooterSite>
+    Yo soy el footer
+  </FooterSite>
+)
+
+export default Footer
---
+++ src/components/web-core/header.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import styled from "styled-components"
+
+const HeaderSite = styled.header`
+  color: red;
+`
+
+const Header = () => (
+  <HeaderSite>
+    Yo soy el header
+  </HeaderSite>
+)
+
+export default Header
